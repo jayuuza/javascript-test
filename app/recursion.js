de7076717a42417e2e2c2e4090661e9c1075aeb4
jayuuza <wassonjonathan@gmail.com>
2017-07-19 00:24:03 +0200

    Fixed recursion

--- app/recursion.js
+++ app/recursion.js
@@ -2,7 +2,32 @@ exports = typeof window === 'undefined' ? global : window;
 
 exports.recursionAnswers = {
   listFiles: function(data, dirName) {
+  	let foundFiles = [];
+  	let getFiles = function(data, dirName){
+      let dir = data["dir"];
+      let files = data["files"];
+      if (files !== undefined || files !== null){
+        for (let i = 0; i < files.length; i++) {
+          if (dirName == dir || dirName === undefined || dirName === null) {
+            if (typeof files[i] === "string"){
+      	      foundFiles.push(files[i]);
+      	    }
+      	    else {
+      	      getFiles(files[i]);
+      	    }
+          }
+          else {
+          	if (typeof files[i] !== "string") {
+          	  getFiles(files[i], dirName);
+          	}
+          }
+      	}
+      }
+  	}
 
+    getFiles(data, dirName);
+
+  	return foundFiles;
   },
 
   permute: function(arr) {
